e54b565a3774573b0e1c86b756f1c546d14cb1bb
Brendan Huddleston <brendanhuddleston18@gmail.com>
2024-03-14 18:56:29 -0500

    made changes

--- listings/models.py
+++ listings/models.py
@@ -1,2 +1,8 @@
 from django.db import models
 
+class Listing(models.Model):
+    user = models.TextField()
+    address = models.TextField()
+    description = models.TextField
+    def __str__(self):
+        return self.address
