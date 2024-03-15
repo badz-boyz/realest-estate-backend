7154753574082aef0f674152b7adda15210e9031
Brendan Huddleston <brendanhuddleston18@gmail.com>
2024-03-15 12:05:17 -0500

    change results to 20

--- listings/views.py
+++ listings/views.py
@@ -34,7 +34,7 @@ def get_listings(request, city=None):
     request_data = {
         'query': query,
         'format': 'JSON',
-        'num_records': 10,
+        'num_records': 20,
         'download': False
     }
     response = requests.post(url, json=request_data, headers=request_headers)
